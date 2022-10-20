c5c9370e07fee25485cbe991deaf384629b29143
Carlos Ferreira <carlos@especializati.com.br>
2022-10-20 10:30:21 -0300

    feat: create new service evaluation

--- src/evaluation/evaluation.module.ts
+++ src/evaluation/evaluation.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EvaluationController } from './evaluation/evaluation.controller';
+import { EvaluationService } from './service/evaluation/evaluation.service';
 
 @Module({
   controllers: [EvaluationController],
+  providers: [EvaluationService],
 })
 export class EvaluationModule {}
