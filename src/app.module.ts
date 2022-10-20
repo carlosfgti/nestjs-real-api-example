d68ce7b584ed7a663f8e552749242017a5e8fb7f
Carlos Ferreira <carlos@especializati.com.br>
2022-10-20 10:21:00 -0300

    feat: create new module, evaluations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EvaluationModule } from './evaluation/evaluation.module';
 
 @Module({
-  imports: [],
+  imports: [EvaluationModule],
   controllers: [AppController],
   providers: [AppService],
 })
