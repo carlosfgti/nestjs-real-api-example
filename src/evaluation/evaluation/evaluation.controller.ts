72b44c43a52c653d6f2543f152d30c12b8116482
Carlos Ferreira <carlos@especializati.com.br>
2022-10-20 10:29:03 -0300

    feat: implements temporary method find all

--- src/evaluation/evaluation/evaluation.controller.ts
+++ src/evaluation/evaluation/evaluation.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('evaluations')
-export class EvaluationController {}
+export class EvaluationController {
+  @Get()
+  findAll(): string {
+    return 'Find All Evaluations';
+  }
+}
