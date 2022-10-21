a2dcac762cd5a33504fba9aa37efea22faa99f52
Carlos Ferreira <carlos@especializati.com.br>
2022-10-21 10:21:58 -0300

    feat: create new course module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EvaluationModule } from './evaluation/evaluation.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [EvaluationModule],
+  imports: [EvaluationModule, CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
