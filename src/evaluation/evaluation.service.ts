7c93487d675f44bb15d5ea5c57e77dc447dce615
Carlos Ferreira <carlos@especializati.com.br>
2022-10-28 11:41:55 -0300

    feat: get all evaluations

--- src/evaluation/evaluation.service.ts
+++ src/evaluation/evaluation.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEvaluationDto } from './dto/create-evaluation.dto';
 import { UpdateEvaluationDto } from './dto/update-evaluation.dto';
 
 @Injectable()
 export class EvaluationService {
+  constructor(private prisma: PrismaService) {}
+
   create(createEvaluationDto: CreateEvaluationDto) {
     return 'This action adds a new evaluation';
   }
 
   findAll() {
-    return `This action returns all evaluation`;
+    return this.prisma.course_evaluation.findMany();
   }
 
   findOne(id: number) {
