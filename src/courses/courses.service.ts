580c2da893bf4d3b9620a169d8d8c1dfedc8ef42
Carlos Ferreira <carlos@especializati.com.br>
2022-10-28 11:24:49 -0300

    refactor: remove actions courses

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCourseDto } from './dto/create-course.dto';
-import { UpdateCourseDto } from './dto/update-course.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CoursesService {
-  create(createCourseDto: CreateCourseDto) {
-    return 'This action adds a new course';
-  }
+  constructor(private prisma: PrismaService) {}
 
   findAll() {
-    return `This action returns all courses`;
+    return this.prisma.courses.findMany();
   }
 
   findOne(id: number) {
     return `This action returns a #${id} course`;
-  }
-
-  update(id: number, updateCourseDto: UpdateCourseDto) {
-    return `This action updates a #${id} course`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} course`;
   }
 }
