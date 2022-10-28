17fd3464e039274af423e2328bc2f6d237821626
Carlos Ferreira <carlos@especializati.com.br>
2022-10-28 11:35:58 -0300

    feat: use prisma in module evaluation

--- src/evaluation/evaluation.module.ts
+++ src/evaluation/evaluation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EvaluationService } from './evaluation.service';
 import { EvaluationController } from './evaluation.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [EvaluationController],
-  providers: [EvaluationService]
+  providers: [EvaluationService],
+  imports: [PrismaModule],
 })
 export class EvaluationModule {}
